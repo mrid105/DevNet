b2ec09b6f09232db24d5f10bf14de431f131fab6
Mridul <mridul1052003@gmail.com>
2026-01-03 17:12:43 -0500

    exploring http methods and testing with postman

--- src/app.js
+++ src/app.js
@@ -2,16 +2,43 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello Devs!")
+app.get("/user", (req, res) => {
+  res.send("Getting your user!");
 });
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from test!")
+app.post("/user", (req, res) => {
+  //Saving data to db
+  res.send("Posted user data successfully!");
 });
 
-app.use("/dashboard",(req,res)=>{
-    res.send("Hello from dashboard!")
+app.delete("/user", (req, res) => {
+  res.send("User deleted successfully");
 });
 
-app.listen(7777, ()=>{console.log("Server is succesfully connected to port 7777.....");});
+app.use("/user", (req, res) => {
+  res.send("User user user");
+});
+
+app.use("/hello/2", (req, res) => {
+  res.send("Hello 2222.");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello hello hello");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello from test!");
+});
+
+app.use("/dashboard", (req, res) => {
+  res.send("Hello from dashboard!");
+});
+
+app.use("/", (req, res) => {
+  res.send("Hello Devs!");
+});
+
+app.listen(7777, () => {
+  console.log("Server is succesfully connected to port 7777.....");
+});
