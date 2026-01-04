02b485d42f6344168f66fc219fa46a0dd26bd524
Mridul <mridul1052003@gmail.com>
2026-01-04 18:53:30 -0500

    playing with multiple route handlers and next()

--- src/app.js
+++ src/app.js
@@ -2,48 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send("Getting your user!");
-});
-
-app.get("/user", (req, res) => {
-  console.log(req.query);
-  res.send("Getting your user!");
-});
-
-app.post("/user", (req, res) => {
-  //Saving data to db
-  res.send("Posted user data successfully!");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("User deleted successfully");
-});
-
-app.use("/user", (req, res) => {
-  res.send("User user user");
-});
-
-app.use("/hello/2", (req, res) => {
-  res.send("Hello 2222.");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello from test!");
-});
-
-app.use("/dashboard", (req, res) => {
-  res.send("Hello from dashboard!");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello Devs!");
-});
+app.use(
+  "/user",
+  [
+    (req, res, next) => {
+      console.log("rh1");
+      //res.send("Response 1!");
+      next();
+    },
+    (req, res, next) => {
+      console.log("rh2");
+      //res.send("Response 2!");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("rh3");
+    res.send("Response 3!");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server is succesfully connected to port 7777.....");
