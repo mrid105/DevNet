bb008efe0bb23d44d20dbb455440f9eb63076fcc
Mridul <mridul1052003@gmail.com>
2026-01-10 19:25:45 -0500

    expiry for cookies and tokens and moving logic for authentication at the correct place

--- src/app.js
+++ src/app.js
@@ -5,108 +5,16 @@ const jwt = require("jsonwebtoken");
 const { connectDB } = require("./config/database");
 const User = require("./models/user");
 const { validateSignUpData } = require("./utils/validation");
+const { userAuth } = require("./middlewares/auth");
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.delete("/user", async (req, res) => {
-  try {
-    const userId = req.body.userId;
-    const user = await User.findByIdAndDelete(userId);
-    if (!user) {
-      res.status(404).send("Could not find user!");
-    } else {
-      res.send("Deleted user successfully!");
-    }
-  } catch (err) {
-    res.status(400).send("Something went wrong delete!" + err.message);
-  }
+
+app.post("/sendConnectionRequest", userAuth, (req, res) => {
+  res.send(req.user.firstName + " sent a connection request.");
 });
 
-app.patch("/user/:userId", async (req, res) => {
-  try {
-    const userId = req.params?.userId;
-    const data = req.body;
-
-    const ALLOWED_UPDATES = ["age", "gender", "skills", "about"];
-    const isUpdateAllowed = Object.keys(data).every((k) =>
-      ALLOWED_UPDATES.includes(k)
-    );
-    if (!isUpdateAllowed) {
-      throw new Error("Update not allowed");
-    }
-    if (data.skills?.length > 15) {
-      throw new Error("More than 15 skills are not allowed.");
-    }
-    const user = await User.findByIdAndUpdate(userId, data, {
-      runValidators: true,
-    });
-    if (!user) {
-      res.status(404).send("Could not find user!");
-    } else {
-      res.send("Updated User successfully");
-    }
-  } catch (err) {
-    res.status(400).send("Something went wrong update!" + err.message);
-  }
-});
-
-app.patch("/userUsingEmail", async (req, res) => {
-  try {
-    const email = req.body.emailId;
-    const firstName = req.body.firstName;
-    const user = await User.updateOne(
-      { emailId: email },
-      { firstName: firstName }
-    );
-    if (!user) {
-      res.status(404).send("Could not find user!");
-    } else {
-      res.send(user);
-    }
-  } catch (err) {
-    res.status(400).send("Something went wrong update email!" + err.message);
-  }
-});
-app.get("/user", async (req, res) => {
-  try {
-    const email = req.body.emailId;
-    const user = await User.findOne({ emailId: email });
-    if (!user) {
-      res.status(404).send("Could not find user!");
-    } else {
-      res.send(user);
-    }
-  } catch (err) {
-    res.status(400).send("Something went wrong!" + err.message);
-  }
-});
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    if (users.length === 0) {
-      res.status(404).send("Could not find user!");
-    } else {
-      res.send(users);
-    }
-  } catch (err) {
-    res.status(400).send("Something went wrong!" + err.message);
-  }
-});
-
-app.get("/userId", async (req, res) => {
-  try {
-    const id = req.body._id;
-    const user = await User.findById(id);
-    if (!user) {
-      res.status(404).send("Could not find user!");
-    } else {
-      res.send(user);
-    }
-  } catch (err) {
-    res.status(400).send("Something went wrong!");
-  }
-});
 app.post("/login", async (req, res) => {
   try {
     const { emailId, password } = req.body;
@@ -116,8 +24,12 @@ app.post("/login", async (req, res) => {
     } else {
       const isPasswordValid = await bcrypt.compare(password, user.password);
       if (isPasswordValid) {
-        const token = await jwt.sign({ _id: user._id }, "DEV@net$2003");
-        const cookie = res.cookie("token", token);
+        const token = await jwt.sign({ _id: user._id }, "DEV@net$2003", {
+          expiresIn: "1d",
+        });
+        const cookie = res.cookie("token", token, {
+          expires: new Date(Date.now() + 7 * 24 * 3600000),
+        });
         res.send("Login successful!!");
       } else {
         throw new Error("Invalid credentials!");
@@ -127,15 +39,10 @@ app.post("/login", async (req, res) => {
     res.status(400).send("Something went wrong! : " + err.message);
   }
 });
-app.get("/profile", async (req, res) => {
+
+app.get("/profile", userAuth, async (req, res) => {
   try {
-    const { token } = req.cookies;
-    if (!token) {
-      throw new Error("Invalid token!");
-    }
-    const decodedMessage = await jwt.verify(token, "DEV@net$2003");
-    const { _id } = decodedMessage;
-    const user = await User.findById(_id);
+    const user = req.user;
     if (!user) {
       throw new Error("User not found!");
     }
@@ -144,6 +51,7 @@ app.get("/profile", async (req, res) => {
     res.status(400).send("Something went wrong! : " + err.message);
   }
 });
+
 app.post("/signup", async (req, res) => {
   try {
     //Validate the user data
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,24 +1,24 @@
-const adminAuth = (req, res, next) => {
-  const token = "xyz";
-  const isAdminAuthorized = token === "xyz";
-
-  console.log("Admin Auth is done!");
-  if (!isAdminAuthorized) {
-    res.send("Unauthorized Request!");
-  } else {
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Invalid Token!!!!");
+    }
+    const decodedMessage = await jwt.verify(token, "DEV@net$2003");
+    const { _id } = decodedMessage;
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found!!");
+    }
+    req.user = user;
     next();
+  } catch (err) {
+    res
+      .status(400)
+      .send("Something went wrong with user authentication: " + err.message);
   }
 };
 
-const userAuth = (req, res, next) => {
-  const token = "xyz";
-  const isUserAuthorized = token === "xyz";
-  console.log("User Auth is done!");
-  if (!isUserAuthorized) {
-    res.send("Unauthorized Request!");
-  } else {
-    next();
-  }
-};
-
-module.exports = { adminAuth, userAuth };
+module.exports = { userAuth };
