943c0765510bac979911ef16dd8d32afcf87078e
Mridul <mridul1052003@gmail.com>
2026-01-12 11:22:45 -0500

    patch password api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
+const User = require("../models/user");
 const profileRouter = express.Router();
 
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
@@ -14,20 +18,35 @@ profileRouter.get("/profile/view", userAuth, async (req, res) => {
   }
 });
 
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    if (!user) {
+      throw new Error("User not found!");
+    }
+
+    const isPasswordValid = await user.validatePassword(req.body.oldPassword);
+    if (!isPasswordValid) {
+      throw new Error("Input is not valid!");
+    }
+    if (!validator.isStrongPassword(req.body.newPassword)) {
+      throw new Error("Enter a strong new password!");
+    }
+    console.log("Old Hash: " + user.password);
+    const passwordHash = await bcrypt.hash(req.body.newPassword, 10);
+    user.password = passwordHash;
+    await user.save();
+    console.log("New Hash: " + user.password);
+    res.send("Password Updated Successfully!");
+  } catch (err) {
+    res.status(400).send("Something went wrong! : " + err.message);
+  }
+});
+
 profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
-    const allowedEdits = [
-      "firstName",
-      "lastName",
-      "age",
-      "gender",
-      "skills",
-      "about",
-    ];
-    const isEditAllowed = Object.keys(req.body).every((field) =>
-      allowedEdits.includes(field)
-    );
+    const isEditAllowed = validateEditProfileData(req);
     if (!isEditAllowed) {
       throw new Error("Invalid edit request!!");
     }
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -10,4 +10,21 @@ const validateSignUpData = (req) => {
     throw new Error("Weak password!");
   }
 };
-module.exports = { validateSignUpData };
+
+const validateEditProfileData = (req) => {
+  const allowedEdits = [
+    "firstName",
+    "lastName",
+    "age",
+    "gender",
+    "skills",
+    "about",
+  ];
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEdits.includes(field)
+  );
+
+  return isEditAllowed;
+};
+
+module.exports = { validateSignUpData, validateEditProfileData };
